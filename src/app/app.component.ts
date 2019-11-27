9f5de1dcb85ce9a088d2c20aee78fd915fb980c1
karguvel kalisekar <karguvel.kalisekar@innoart.local>
2019-11-27 18:14:46 +0530

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TestGithub KM'
+test = "Welcome";
+console.log("test");
+console.log("test1");
+console.log("test2");
+}
